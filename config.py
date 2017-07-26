71032738f4079fa6e7c526659da9e2a0b2d897e6
LuisMDlab <luisfelipeprf@gmail.com>
2017-07-26 15:38:54 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Dicionário de Tokens de cada "user" do Time
+#Token de acesso com privilégios somente de repositório
+token_dict = {
+        'token_leo': 'Token',
+        'token_luis': 'Token',
+        'token_wery': 'Token',
+        'token_rodrigo': 'Token',
+        'token_eduardo':'Token',
+        'token_julianny': 'Token',
+        'token_wallison': 'Token',
+        'token_andre': 'Token',
+        'token_mbruno':'Token',
+        'token_marcel':'Token'}
